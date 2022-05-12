f510b9b7f318e514207574101067106ee37028ce
andyhsia020 <58779906+andyhsia020@users.noreply.github.com>
2022-05-12 22:41:27 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,14 +2,14 @@
 
 # 配置环境
 npm install -g hexo-cli
-mkdir hexo
-cd hexo
+mkdir victorypicker
+cd victorypicker
 hexo init
 npm install
 rm -rf source/_posts
 mkdir -p source/_posts
 
-# 安装主题(你们可以改成自己喜欢的主题)
+# 安装主题
 git clone https://github.com/theme-next/hexo-theme-next themes/next
 
 # 使用新的渲染器
@@ -22,7 +22,7 @@ npm i hexo-image-link --save
 cd ../
 cp -r `ls | grep -v hexo | grep -v config.yml | xargs` hexo/source/_posts
 # 复制配置文件
-cp config.yml hexo
+cp config.yml victorypicker
 
 # 开始构建
-cd hexo && hexo g --config config.yml
+cd victorypicker && victorypicker g --config config.yml
